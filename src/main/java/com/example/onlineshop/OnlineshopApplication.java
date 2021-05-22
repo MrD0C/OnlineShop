2b25cbff56d7a41884bb290e28893d3dfb880d78
Daniil Kudiyarov <daniilkudiyarovsocial@gmail.com>
2021-05-22 14:00:29 +0400

    Added test entities (not completed)

--- src/main/java/com/example/onlineshop/OnlineshopApplication.java
+++ src/main/java/com/example/onlineshop/OnlineshopApplication.java
@@ -8,10 +8,7 @@ public class OnlineshopApplication {
 
     //TODO добавить тестовые сущности при запуске
     //TODO добавить логгер
-    //TODO закончить Item
     //TODO создать класс Address
-    //todo отловить ошибку сериализации даты
-    //TODO сделать общий класс для построения ответа ошкибки?
     public static void main(String[] args) {
         SpringApplication.run(OnlineshopApplication.class, args);
     }
---
+++ src/main/resources/db/populate-H2.sql
@@ -0,0 +1,3 @@
+/*INSERT INTO CUSTOMERS VALUES (id,first name,last name,birth date(yyyy-MM-dd),gender,shipping address) */
+INSERT INTO CUSTOMERS VALUES (1,'Даниил','Кудияров','2000-07-05',0,'ул.Московское шоссе,д.177,кв.64');
+INSERT INTO CUSTOMERS VALUES (2,'Иван','Иванов','1999-04-20',0,'ул.Зубчаниновское шоссе,д.25,кв.1');
